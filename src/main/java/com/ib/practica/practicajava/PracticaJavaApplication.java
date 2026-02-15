c4455f4f30bed2784bd4a0660b301e4e7e784044
BABEL.msanchez <BABEL.msanchez@iberia.es>
2026-02-15 19:01:37 +0100

    Init project

---
+++ src/main/java/com/ib/practica/practicajava/PracticaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.ib.practica.practicajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaJavaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PracticaJavaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ib/practica/practicajava/PracticaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ib.practica.practicajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaJavaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
